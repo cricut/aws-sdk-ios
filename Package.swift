3cbe4fb923cc56942c5041105b12b0ed0e6e3b71
Andrew Cobb <acobb@cricut.com>
2020-10-21 15:54:09 -0600

    update tag

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AWSKinesis.xcframework",
-            url: "https://github.com/cricut/aws-sdk-ios/releases/download/cobbal/kinesis-only-2.18.0/AWSKinesis.xcframework.zip",
+            url: "https://github.com/cricut/aws-sdk-ios/releases/download/2.18.0_kinesis_only/AWSKinesis.xcframework.zip",
             checksum: "18f3b34f07918982aa3ba3e9c05422787e3d364bb9ef5bf5d74c7552de0490c0"
         ),
         .binaryTarget(
             name: "AWSCore.xcframework",
-            url: "https://github.com/cricut/aws-sdk-ios/releases/download/cobbal/kinesis-only-2.18.0/AWSCore.xcframework.zip",
+            url: "https://github.com/cricut/aws-sdk-ios/releases/download/2.18.0_kinesis_only/AWSCore.xcframework.zip",
             checksum: "cb10781bf4c62963ad61065eb4bcbf75fe1e396b2fb551e7d7e43369d8a80c8e"
         ),
     ]
